c8bdc19908d0d05d04a90a562569440ca7ad9d9a
nevarwin <raven.solis@philcentro.ph>
2024-09-09 23:23:34 +0800

    added revalidate

--- app/about/page.js
+++ app/about/page.js
@@ -2,20 +2,25 @@ import Image from 'next/image';
 import Link from 'next/link';
 import image1 from '@/public/about-1.jpg';
 import image2 from '@/public/about-2.jpg';
+import { getCabins } from '../_lib/data-service';
+
+export const revalidate = 3600;
 
 export const metadata = {
     title: 'About',
 };
 
-export default function Page() {
+export default async function Page() {
+    const cabins = await getCabins();
+
     return (
-        <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
-            <div className="col-span-3">
-                <h1 className="text-4xl mb-10 text-accent-400 font-medium">
+        <div className='grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center'>
+            <div className='col-span-3'>
+                <h1 className='text-4xl mb-10 text-accent-400 font-medium'>
                     Welcome to The Wild Oasis
                 </h1>
 
-                <div className="space-y-8">
+                <div className='space-y-8'>
                     <p>
                         Where nature&apos;s beauty and comfortable living blend
                         seamlessly. Hidden away in the heart of the Italian
@@ -25,11 +30,11 @@ export default function Page() {
                         enjoying simple pleasures with family.
                     </p>
                     <p>
-                        Our 8 luxury cabins provide a cozy base, but the real
-                        freedom and peace you&apos;ll find in the surrounding
-                        mountains. Wander through lush forests, breathe in the
-                        fresh air, and watch the stars twinkle above from the
-                        warmth of a campfire or your hot tub.
+                        Our {cabins.length} luxury cabins provide a cozy base,
+                        but the real freedom and peace you&apos;ll find in the
+                        surrounding mountains. Wander through lush forests,
+                        breathe in the fresh air, and watch the stars twinkle
+                        above from the warmth of a campfire or your hot tub.
                     </p>
                     <p>
                         This is where memorable moments are made, surrounded by
@@ -40,33 +45,33 @@ export default function Page() {
                 </div>
             </div>
 
-            <div className="col-span-2">
+            <div className='col-span-2'>
                 <Image
                     src={image1}
                     // fill
-                    placeholder="blur"
+                    placeholder='blur'
                     quality={80}
-                    alt="Family sitting around a fire pit in front of cabin"
+                    alt='Family sitting around a fire pit in front of cabin'
                 />
             </div>
 
-            <div className="col-span-2 relative aspect-square">
+            <div className='col-span-2 relative aspect-square'>
                 <Image
-                    src="/about-2.jpg"
+                    src='/about-2.jpg'
                     fill
                     // placeholder="blur"
                     quality={80}
-                    className="object-cover"
-                    alt="Family that manages The Wild Oasis"
+                    className='object-cover'
+                    alt='Family that manages The Wild Oasis'
                 />
             </div>
 
-            <div className="col-span-3">
-                <h1 className="text-4xl mb-10 text-accent-400 font-medium">
+            <div className='col-span-3'>
+                <h1 className='text-4xl mb-10 text-accent-400 font-medium'>
                     Managed by our family since 1962
                 </h1>
 
-                <div className="space-y-8">
+                <div className='space-y-8'>
                     <p>
                         Since 1962, The Wild Oasis has been a cherished
                         family-run retreat. Started by our grandparents, this
@@ -86,8 +91,8 @@ export default function Page() {
 
                     <div>
                         <Link
-                            href="/cabins"
-                            className="inline-block mt-4 bg-accent-500 px-8 py-5 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+                            href='/cabins'
+                            className='inline-block mt-4 bg-accent-500 px-8 py-5 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all'
                         >
                             Explore our luxury cabins
                         </Link>
