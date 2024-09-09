7f70159c9b652beffd7f1e2b176de9fd01d96cd7
nevarwin <raven.solis@philcentro.ph>
2024-09-09 18:58:37 +0800

    added suspense

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,20 +1,18 @@
-import CabinCard from '../_components/CabinCard';
-import { getCabins } from '../_lib/data-service';
+import { Suspense } from 'react';
+import CabinLists from '../_components/CabinLists';
+import Spinner from '../_components/Spinner';
 
 export const metadata = {
     title: 'Cabins',
 };
 
-export default async function Page() {
-    // CHANGE
-    const cabins = await getCabins();
-
+export default function Page() {
     return (
         <div>
-            <h1 className="text-4xl mb-5 text-accent-400 font-medium">
+            <h1 className='text-4xl mb-5 text-accent-400 font-medium'>
                 Our Luxury Cabins
             </h1>
-            <p className="text-primary-200 text-lg mb-10">
+            <p className='text-primary-200 text-lg mb-10'>
                 Cozy yet luxurious cabins, located right in the heart of the
                 Italian Dolomites. Imagine waking up to beautiful mountain
                 views, spending your days exploring the dark forests around, or
@@ -23,13 +21,9 @@ export default async function Page() {
                 perfect spot for a peaceful, calm vacation. Welcome to paradise.
             </p>
 
-            {cabins.length > 0 && (
-                <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-                    {cabins.map((cabin) => (
-                        <CabinCard cabin={cabin} key={cabin.id} />
-                    ))}
-                </div>
-            )}
+            <Suspense fallback={<Spinner />}>
+                <CabinLists />
+            </Suspense>
         </div>
     );
 }
