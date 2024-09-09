4ee4aa58043f101f43bea8e476f0f1803af0395c
nevarwin <raven.solis@philcentro.ph>
2024-09-09 13:35:55 +0800

    added styling

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -2,25 +2,33 @@ import Link from 'next/link';
 
 export default function Navigation() {
     return (
-        <div>
-            <ul>
-                <ol>
-                    {' '}
-                    <Link href={'/'}>Home</Link>
-                </ol>
-                <ol>
-                    {' '}
-                    <Link href={'/cabins'}>Cabins</Link>
-                </ol>
-                <ol>
-                    {' '}
-                    <Link href={'/account'}>Account</Link>
-                </ol>
-                <ol>
-                    {' '}
-                    <Link href={'/about'}>About</Link>
-                </ol>
+        <nav className="z-10 text-xl">
+            <ul className="flex gap-16 items-center">
+                <li>
+                    <Link
+                        href="/cabins"
+                        className="hover:text-accent-400 transition-colors"
+                    >
+                        Cabins
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href="/about"
+                        className="hover:text-accent-400 transition-colors"
+                    >
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href="/account"
+                        className="hover:text-accent-400 transition-colors"
+                    >
+                        Guest area
+                    </Link>
+                </li>
             </ul>
-        </div>
+        </nav>
     );
 }
