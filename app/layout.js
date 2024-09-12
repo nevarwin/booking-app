5a5661699730af66696fc6430c18bffdbe0ecca0
nevarwin <raven.solis@philcentro.ph>
2024-09-12 10:39:31 +0800

    added context provider

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Navigation from '@/app/_components/Navigation';
 import '@/app/_styles/globals.css';
 import { Roboto_Flex } from 'next/font/google';
 import Header from './_components/Header';
+import { ReservationProvider } from './_components/ReservationProvider';
 
 export const metadata = {
     // title: 'Booking App',
@@ -20,14 +21,16 @@ const roboto = Roboto_Flex({
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang='en'>
             <body
                 className={`${roboto.className} antialiased bg-primary-900 text-primary-100 min-h-screen flex flex-col`}
             >
                 <Header />
 
-                <div className="flex-1 px-8 py-12 grid">
-                    <main className="max-w-7xl mx-auto w-full">{children}</main>
+                <div className='flex-1 px-8 py-12 grid'>
+                    <main className='max-w-7xl mx-auto w-full'>
+                        <ReservationProvider>{children}</ReservationProvider>
+                    </main>
                 </div>
             </body>
         </html>
