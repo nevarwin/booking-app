cfe84ce2d02e72dd258f44cdd602c350ae870378
nevarwin <raven.solis@philcentro.ph>
2024-09-12 10:59:50 +0800

    added resetRange

--- app/_components/ReservationProvider.js
+++ app/_components/ReservationProvider.js
@@ -1,16 +1,17 @@
 'use client';
 
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
 
 const ReservationContext = createContext();
 
 const initialState = { from: undefined, to: undefined };
 
 function ReservationProvider({ children }) {
-    const { range, setRange } = useState(initialState);
+    const [range, setRange] = useState(initialState);
+    const resetRange = () => setRange(initialState);
 
     return (
-        <ReservationContext.Provider value={{ range, setRange }}>
+        <ReservationContext.Provider value={{ range, setRange, resetRange }}>
             {children}
         </ReservationContext.Provider>
     );
@@ -19,7 +20,7 @@ function ReservationProvider({ children }) {
 function useReservation() {
     const context = useContext(ReservationContext);
     if (context === undefined)
-        throw new Error('Context used outside the provider');
+        throw new Error('Context was used outside provider');
     return context;
 }
 
