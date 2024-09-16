5c0d4da0fd6f1f15cb6797e540ed929def06eecc
nevarwin <raven.solis@philcentro.ph>
2024-09-16 23:09:43 +0800

    added server actions via form

--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,12 @@
+import { auth } from '../_lib/auth';
+
 export const metadata = {
     title: 'Accounts',
 };
 
-export default function Page() {
-    return (
-        <h2 className="font-semibold text-2xl text-accent-400 mb-4">Welcome</h2>
-    );
+export default async function Page() {
+    const session = await auth();
+    const firstName = session.user.name.split(' ').at(0);
+
+    return <h2 className='font-semibold text-2xl text-accent-400 mb-4'>Welcome, {firstName}</h2>;
 }
