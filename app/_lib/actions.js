d69dfbd7bb1137606d23eb585baae81c855a7c36
nevarwin <raven.solis@philcentro.ph>
2024-10-02 23:32:38 +0800

    added a server action to the button

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { revalidatePath } from 'next/cache';
 import { auth, signIn, signOut } from './auth';
 import supabase from './supabase';
+import { getBookings } from './data-service';
 
 export async function updateGuest(formData) {
     const session = await auth();
@@ -25,6 +26,27 @@ export async function updateGuest(formData) {
     if (error) throw new Error('Guest could not be updated');
 
     revalidatePath('/account/profile');
+}
+
+export async function deleteReservation(bookingId) {
+    const session = await auth();
+
+    if (!session) throw new Error('You must be logged in.');
+
+    const guestBookings = await getBookings(session.user.guestId);
+    const bookingIds = guestBookings.map((booking) => booking.id);
+
+    if (!bookingIds.includes(bookingId))
+        throw new Error('You cannot delete this booking!');
+
+    const { error } = await supabase
+        .from('bookings')
+        .delete()
+        .eq('id', bookingId);
+
+    if (error) throw new Error('Booking could not be deleted');
+
+    revalidatePath('/account/reservations');
 }
 
 export async function signInAction() {
