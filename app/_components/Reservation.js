05a3c6c9fc4fd437b457bfb4c624824184f2a91e
nevarwin <raven.solis@philcentro.ph>
2024-10-17 23:20:14 +0800

    added business logic to dateselector

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -6,14 +6,25 @@ import { auth } from '../_lib/auth';
 import LoginMessage from './LoginMessage';
 
 async function Reservation({ cabin }) {
-    const [settings, bookedDates] = await Promise.all([getSettings(), getBookedDatesByCabinId(cabin.id)]);
+    const [settings, bookedDates] = await Promise.all([
+        getSettings(),
+        getBookedDatesByCabinId(cabin.id),
+    ]);
 
     const session = await auth();
 
     return (
         <div className='grid grid-cols-2 border border-primary-800 min-h-[400px]'>
-            <DateSelector cabin={cabin} settings={settings} bookedDates={bookedDates} />
-            {session?.user ? <ReservationForm cabin={cabin} user={session.user} /> : <LoginMessage />}
+            <DateSelector
+                cabin={cabin}
+                settings={settings}
+                bookedDates={bookedDates}
+            />
+            {session?.user ? (
+                <ReservationForm cabin={cabin} user={session.user} />
+            ) : (
+                <LoginMessage />
+            )}
         </div>
     );
 }
