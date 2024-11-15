d5057ce73b526009ff25ad3be37f13a58ef90ed5
nevarwin <raven.solis@philcentro.ph>
2024-11-15 10:18:24 +0800

    revoked keys

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wnujdvuddyvdibkjxjfu.supabase.co';
-const supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndudWpkdnVkZHl2ZGlia2p4amZ1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNTg2NzIwOSwiZXhwIjoyMDQxNDQzMjA5fQ.39j9z9ooFN2SD-N5FGGCXxd6666LYziHi7KmB67fy7U';
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+);
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
